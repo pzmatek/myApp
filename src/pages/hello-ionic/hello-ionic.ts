60b680a82656c98daaa4655cc13480cbacc90b50
unknown <mw@symbio.com.pl>
2018-10-08 21:32:44 +0200

    zmiana ikon i ekranu app.html

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser';
 
 @Component({
   selector: 'page-hello-ionic',
@@ -7,16 +7,17 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 })
 export class HelloIonicPage {
 
+  options: InAppBrowserOptions = {
+    location: 'no',
+    disallowoverscroll: 'no', //iOS only 
+    toolbar: 'no', //iOS only 
+    enableViewportScale: 'no', //iOS only 
+    allowInlineMediaPlayback: 'no',//iOS only 
+    presentationstyle: 'pagesheet',//iOS only 
+    fullscreen: 'yes',//Windows only    
+  };
+
   constructor(private inAppBrowser: InAppBrowser) {
-   // const browser = this.inAppBrowser.create('http://panel.kuracja.testowa.eu:8000','_blank');
-    
-
-
-  }
-
-  openWebpage(url: string) {
-    const browser = this.inAppBrowser.create(url);
-
-    //browser.show();
+    this.inAppBrowser.create('http://panel.kuracja.testowa.eu:8000', '_self', this.options);
   }
 }
